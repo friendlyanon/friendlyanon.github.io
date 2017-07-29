6e1e8e4ee2053d129da0b53412e1694245d6f6cb
friendlyanon <1736896+friendlyanon@users.noreply.github.com>
2017-07-29 14:43:00 +0200

    fixed everything finally

--- gif-engine-js/script/example.js
+++ gif-engine-js/script/example.js
@@ -1,25 +1,38 @@
 (function(d) {
-  let current = 0;
   let frameCount = 0;
   let index = -1;
-  let frames = [];
-  let delays = [];
+  let compiledFrames = void 0;
+  let delays = void 0;
+  let disposals = void 0;
+  let transparentColorIndexes = void 0;
   let width = 0;
   let height = 0;
   const canvas = d.createElement("canvas");
   const ctx = canvas.getContext("2d");
-  const track = () => performance.now();
-  let time = 0;
-  const display = function() {
+  const drawFrame = function() {
     if (!(index < frameCount)) index = 0;
-    let now = track();
-    if (now - time > delays[index]) {
-      ctx.clearRect(0, 0, width, height);
-      ctx.putImageData(...frames[index]);
-      time = now;
-      ++index;
+    const [{ data: imgData, width: w, height: h }, left, top] = compiledFrames[index];
+    switch(disposals[index]) {
+     case 2:
+      ctx.clearRect(left, top, w, h);
+      break;
+     case 3:
+      ctx.putImageData(...compiledFrames[(index === 0 ? frameCount : index) - 1]);
+      break;
     }
-    requestAnimationFrame(display);
+    let current = ctx.getImageData(left, top, w, h);
+    for (let i = 0; w * h > i; ++i) {
+      let offset = i * 4;
+      if (imgData[offset + 3] === 0)
+        continue;
+      current.data[offset]   = imgData[offset];
+      current.data[++offset] = imgData[offset];
+      current.data[++offset] = imgData[offset];
+      current.data[++offset] = imgData[offset];
+    }
+    ctx.putImageData(current, left, top);
+    setTimeout(drawFrame, Math.max(delays[index] - 5, 0));
+    ++index;
   };
   d.querySelector("button").addEventListener("click", e => {
     e.currentTarget.remove();
@@ -28,18 +41,21 @@
       .then(GIF)
       .then(async (o, err) => {
         frameCount = o.frames.length;
-        frames = new Array(frameCount);
+        compiledFrames = new Array(frameCount);
         delays = o.frames.map(frameObj =>
           frameObj.graphicExtension.delay
         );
+        disposals = o.frames.map(frameObj =>
+          frameObj.graphicExtension.disposalMethod
+        );
         ({ width, height } = o.descriptor);
         while(++index < frameCount)
-          frames[index] = await o.toImageData(index);
+          compiledFrames[index] = await o.toImageData(index);
         canvas.width = width;
         canvas.height = height;
         canvas.setAttribute("style", `height: ${height}px; width: ${width}px;`);
         d.body.children[0].appendChild(canvas);
-        requestAnimationFrame(display);
+        requestAnimationFrame(drawFrame);
       });
   }, { once: true });
 }(document));
