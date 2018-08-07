f9a4a324d6dccbc539a847fb67dc9bb2bf937336
friendlyanon <1736896+friendlyanon@users.noreply.github.com>
2018-08-07 14:59:48 +0300

    Add files via upload

--- amiami-crawler/AmiAmi_Crawler_-_Alternative_XHR.user.js
+++ amiami-crawler/AmiAmi_Crawler_-_Alternative_XHR.user.js
@@ -3,13 +3,18 @@
 // @description Alternative way to make network requests for AmiAmi Crawler
 // @namespace   friendlyanon
 // @include     https://friendlyanon.github.io/amiami-crawler/
-// @version     3
+// @version     4
 // @grant       GM_xmlhttpRequest
+// @grant       GM.xmlHttpRequest
 // ==/UserScript==
 
-/* global GM_xmlhttpRequest */
+/* global GM_xmlhttpRequest, GM */
 
 "use strict";
+
+const xhr = typeof GM !== "undefined" ?
+  GM.xmlHttpRequest.bind(GM) :
+  GM_xmlhttpRequest;
 
 function onload(e) {
   document.dispatchEvent(new CustomEvent("amiami-res", { detail: e.responseText }));
@@ -19,7 +24,7 @@ document.addEventListener("amiami-xhr", function(e) {
   if (!e.detail) {
     return document.dispatchEvent(new CustomEvent("amiami-res"));
   }
-  GM_xmlhttpRequest({
+  xhr({
     method: "GET",
     url: e.detail,
     headers: { "x-user-key": "amiami_dev" },
